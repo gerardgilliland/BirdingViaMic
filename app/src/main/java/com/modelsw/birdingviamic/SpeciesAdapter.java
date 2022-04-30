9fea4230f147ff699e6f0c7b2e314cfe010da404
gerardgilliland <gerardg@modelsw.com>
2022-04-29 19:47:38 -0600

    rebase starting at 60f.129

--- app/src/main/java/com/modelsw/birdingviamic/SpeciesAdapter.java
+++ app/src/main/java/com/modelsw/birdingviamic/SpeciesAdapter.java
@@ -14,24 +14,24 @@ import android.widget.TextView;
 
 
 public class SpeciesAdapter extends BaseAdapter implements SectionIndexer {
-	private static final String TAG = "SpeciesAdapter";    
+    private static final String TAG = "SpeciesAdapter";
     private Activity activity;
     private String[] data;
-	public static ViewHolder holder;
+    public static ViewHolder holder;
     private static LayoutInflater inflater=null;
-	private String mSections = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private String mSections = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    
+
     public SpeciesAdapter(Activity a, String[] d) {
         activity = a;
-        data = d;        
+        data = d;
         inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
     }
 
     public int getCount() {
-        return SpeciesList.speciesDbLen;  
-        
+        return SpeciesList.speciesDbLen;
+
     }
 
     public Object getItem(int position) {
@@ -45,38 +45,40 @@ public class SpeciesAdapter extends BaseAdapter implements SectionIndexer {
 
     public View getView(int position, View convertView, ViewGroup parent) {
         View vi=convertView;
- //   	Log.d(TAG, "getView outside of OnClickListener position:" + position);
+        //   	Log.d(TAG, "getView outside of OnClickListener position:" + position);
         if(vi==null) {
 //        	Log.d(TAG, "getView vi is null");
             vi = inflater.inflate(R.layout.species_list, null);
-        	holder = new ViewHolder();
+            holder = new ViewHolder();
             holder.text = (TextView) vi.findViewById(R.id.text);
             holder.check = (CheckBox) vi.findViewById(R.id.check);
             vi.setTag(holder);
         } else {
- //       	Log.d(TAG, "getView vi is NOT null");
-            holder = (ViewHolder) vi.getTag();        	
+            //       	Log.d(TAG, "getView vi is NOT null");
+            holder = (ViewHolder) vi.getTag();
         }
         holder.text.setText(SpeciesList.speciesCombined[position]);
         holder.check.setChecked(SpeciesList.chk[position]);
         holder.check.setId(position);  // this setId position is different than getId below ???
         holder.check.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-		    	int pos = v.getId();
-		    	SpeciesList.chk[pos] = !SpeciesList.chk[pos]; 
-		    	Log.d(TAG, "getView onClick position:" + pos + " ck:" + SpeciesList.chk[pos]);
-		    	for (int i=0; i<SpeciesList.speciesDbLen; i++) {
-		    		if (SpeciesList.chk[i] == true) {
-		    			SpeciesList.selectedSpecies = i;  // do i use this ??
-     	    	   	 	Main.existingRef = Main.speciesRef[i];     	    	   	 	
-     	    	   	 	Main.specOffset = i;
-     	    	   	 	Log.d(TAG, "selectedSpecies:" + SpeciesList.speciesCombined[i] + " Ref:" + Main.existingRef);
-		    		}    		    		
-		    	}
+                int pos = v.getId();
+                SpeciesList.chk[pos] = !SpeciesList.chk[pos];
+                Log.d(TAG, "getView onClick position:" + pos + " ck:" + SpeciesList.chk[pos]);
+                for (int i=0; i<SpeciesList.speciesDbLen; i++) {
+                    if (SpeciesList.chk[i] == true) {
+                        // save this in Main so the specie can be used in Songs Name
+                        SpeciesList.selectedSpecies = i;
+                        Main.specieRefSelected = Main.speciesRef[i];
+                        Main.specOffset = i;
+                        Main.specieSelected = SpeciesList.speciesCombined[i];
+                        Log.d(TAG, "selectedSpecies:" + Main.specieSelected + " Ref:" + Main.specieRefSelected);
+                    }
+                }
 
             }
 
-        }); 
+        });
         return vi;
     }
 
@@ -109,5 +111,3 @@ public class SpeciesAdapter extends BaseAdapter implements SectionIndexer {
     }
 
 }
-
-
