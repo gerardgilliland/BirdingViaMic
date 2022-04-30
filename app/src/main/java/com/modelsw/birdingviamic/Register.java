c7914df8fd08342b2c35413319527f650294f10e
gerardgilliland <gerardg@modelsw.com>
2022-04-29 19:17:53 -0600

    rebase starting at 60f.129

--- app/src/main/java/com/modelsw/birdingviamic/Register.java
+++ app/src/main/java/com/modelsw/birdingviamic/Register.java
@@ -26,89 +26,89 @@ import android.widget.TextView;
 import android.content.pm.PackageInfo;
 
 public class Register extends AppCompatActivity implements OnClickListener {
-    String TAG = "Register";
+	String TAG = "Register";
 	String qry = "";
 	Cursor rs; // I think of Cursor as Record Set
 	EditText comments;
-	String dateFmt; 
+	String dateFmt;
 	Intent emailIntent;
 	String info;
 	long iNow;
 	TextView versionInfo;
-    Toolbar toolbar;
+	Toolbar toolbar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register);
 
-        // action bar toolbar
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_action_back);
-        toolbar.setLogo(R.drawable.treble_clef_linen);
-        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.teal));
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.d(TAG, "Navigation Icon tapped");
-                finish();
-            }
-        });
+		// action bar toolbar
+		toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		toolbar.setNavigationIcon(R.drawable.ic_action_back);
+		toolbar.setLogo(R.drawable.treble_clef_linen);
+		toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.teal));
+		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Log.d(TAG, "Navigation Icon tapped");
+				finish();
+			}
+		});
 
-        iNow = System.currentTimeMillis();
-		String format = "yyyy-MM-dd HH:mm";        	
+		iNow = System.currentTimeMillis();
+		String format = "yyyy-MM-dd HH:mm";
 		SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
 		dateFmt = sdf.format(iNow);
-		info =  "DateTime:" + dateFmt 
+		info =  "DateTime:" + dateFmt
 				+ "\nManufacturer:" + getManufacturer()
-		 		+ "\nModel:" + getModel()
-		 		+ "\nAndroidVersion:" + getAndroidVersion()
-		 		+ "\nAndroidRelease:" + getAndroidRelease()
-		 		+ "\nBirdingViaMic:" + getBirdingViaMicVersion(this)
+				+ "\nModel:" + getModel()
+				+ "\nAndroidVersion:" + getAndroidVersion()
+				+ "\nAndroidRelease:" + getAndroidRelease()
+				+ "\nBirdingViaMic:" + getBirdingViaMicVersion(this)
 				+ "\nIocVersion:" + getIocVersion()
 				+ "\nComments:";
-		
-	    versionInfo = (TextView) findViewById(R.id.version_info);
-	    versionInfo.setText(info);
-	    
-	    comments = (EditText) findViewById(R.id.comments);
-	    View registerNowButton = findViewById(R.id.register_now_button);
-	    registerNowButton.setOnClickListener(this);
 
-	    
+		versionInfo = (TextView) findViewById(R.id.version_info);
+		versionInfo.setText(info);
+
+		comments = (EditText) findViewById(R.id.comments);
+		View registerNowButton = findViewById(R.id.register_now_button);
+		registerNowButton.setOnClickListener(this);
+
+
 	}
 
 	public void onClick(View v) {
-    	switch (v.getId()) {
-    	case R.id.register_now_button:
-			emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "birdingviamic@modelsw.com", null));
-    	    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Register_BirdingViaMic");
-    	    emailIntent.putExtra(Intent.EXTRA_TEXT, info + comments.getText() + "\n");
-    		startActivityForResult(emailIntent, 1);    		
-    		break;
-    	} // switch
-	}	  
+		switch (v.getId()) {
+			case R.id.register_now_button:
+				emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "birdingviamic@modelsw.com", null));
+				emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Register_BirdingViaMic");
+				emailIntent.putExtra(Intent.EXTRA_TEXT, info + comments.getText() + "\n");
+				startActivityForResult(emailIntent, 1);
+				break;
+		} // switch
+	}
 
 
 	public static String getAndroidVersion() {
-	    int iVer = Build.VERSION.SDK_INT;
+		int iVer = Build.VERSION.SDK_INT;
 		return String.format("%d", iVer);
 	}
-	  
+
 	public static String getAndroidRelease() {
-	    return Build.VERSION.RELEASE;
+		return Build.VERSION.RELEASE;
 	}
-	  
+
 	public static String getManufacturer() {
-		  return Build.MANUFACTURER;
-	  }
-	  
+		return Build.MANUFACTURER;
+	}
+
 	public static String getModel() {
-		  return Build.MODEL;
-	  }
-	  
+		return Build.MODEL;
+	}
+
 	public static String getBirdingViaMicVersion(Context ctx) {
-	    return ctx.getResources().getString(R.string.app_version_name);
+		return ctx.getResources().getString(R.string.app_version_name);
 	}
 
 	public String getIocVersion() {
