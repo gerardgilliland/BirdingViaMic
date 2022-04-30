696cbd46ebafa1356a80a251ae574928cbaa6b37
gerardgilliland <gerardg@modelsw.com>
2022-04-30 10:47:40 -0600

    rebase starting at 60g.130

---
+++ app/build/generated/source/buildConfig/debug/com/modelsw/birdingviamic/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.modelsw.birdingviamic;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.modelsw.birdingviamic";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 130;
+  public static final String VERSION_NAME = "60g.130";
+}
